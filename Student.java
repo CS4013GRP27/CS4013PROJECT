a8496516f9c8baae094f8bdd642f199ebb804db4
DaraghRoche7 <150368425+DaraghRoche7@users.noreply.github.com>
2023-11-27 17:09:56 +0000

    Update Student.java

--- Student.java
+++ Student.java
@@ -9,7 +9,7 @@ public class Student {
    private String address;
    private Program program;
    private List<ModuleGrade> modules;
-   private double gpa;
+   private double qca;
 
 
    public Student(int studentId, String name, String email, String address, Program program) {
@@ -136,8 +136,8 @@ public class Student {
    }
 
 
-   public double getGpa() {
-       return gpa;
+   public double getQca() {
+       return qca;
    }
 
 
