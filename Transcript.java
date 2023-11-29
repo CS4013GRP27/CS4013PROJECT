e4d342d4f4d8aea3b0c15512c2b32ecba2e1ef97
Johan <117460414+johan13493@users.noreply.github.com>
2023-11-29 00:30:21 +0000

    Add files via upload

---
+++ Transcript.java
@@ -0,0 +1,27 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Transcript {
+
+    private Student student;
+    private Map<Module, String> moduleGrades;
+
+    public Transcript(Student student) {
+        this.student = student;
+        this.moduleGrades = new HashMap<>();
+    }
+
+    public void generateTranscript() {
+        // logic to generate transcript
+    }
+
+    public void printTranscript() {
+        // print transcript details
+    }
+
+    public void updateModuleGrade(Module module, String grade) {
+        moduleGrades.put(module, grade);
+    }
+
+    // other methods like finalizeGrades() etc
+}
