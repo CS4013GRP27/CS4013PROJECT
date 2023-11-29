1a20f7b7139f388ce4c4ff54a0d8e7ac25847d76
AlexSidorov12 <lex.rodis@gmail.com>
2023-11-29 12:17:04 +0000

    -

--- Transcript.java
+++ Transcript.java
@@ -1,26 +1,40 @@
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Transcript {
-
     private Student student;
-    private Map<Module, String> moduleGrades;
+    private List<ModuleGrade> moduleGrades;
 
     public Transcript(Student student) {
         this.student = student;
-        this.moduleGrades = new HashMap<>();
+        this.moduleGrades = new ArrayList<>();
     }
 
-    public void generateTranscript() {
-        // logic to generate transcript
+    public void addModuleGrade(Module module, String grade) {
+        ModuleGrade moduleGrade = new ModuleGrade(module, grade);
+        moduleGrades.add(moduleGrade);
     }
 
     public void printTranscript() {
-        // print transcript details
+        System.out.println("Transcript for " + student.getName() + ":");
+        for (ModuleGrade moduleGrade : moduleGrades) {
+            System.out.println(moduleGrade.getModule().getName() + ": " + moduleGrade.getGrade());
+        }
     }
 
-    public void updateModuleGrade(Module module, String grade) {
-        moduleGrades.put(module, grade);
+    public Student getStudent() {
+        return student;
     }
 
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public List<ModuleGrade> getModuleGrades() {
+        return moduleGrades;
+    }
+
+    public void setModuleGrades(List<ModuleGrade> moduleGrades) {
+        this.moduleGrades = moduleGrades;
+    }
 }
