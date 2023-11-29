3dfa1d719a0e747d509103724616436517049b7e
DaraghRoche7 <150368425+DaraghRoche7@users.noreply.github.com>
2023-11-29 11:36:03 +0000

    Small changes to ModuleGrade.java

--- ModuleGrade.java
+++ ModuleGrade.java
@@ -2,11 +2,12 @@
 public class ModuleGrade {
 String grade;
 int creditValue;
+String moduleCode;
 
-
-	public ModuleGrade(String grade, int creditValue) {
+	public ModuleGrade(String moduleCode, String grade, int creditValue) {
 	this.grade = grade;
 	this.creditValue = creditValue;
+	this.moduleCode= moduleCode;
 	}
 	
 	public String getGrade() {
@@ -24,6 +25,11 @@ int creditValue;
     public void setCreditValue(int creditValue) {
         this.creditValue = creditValue;
     }
+	public String getModuleCode() {
+		return moduleCode;
+	}
 	
-	
+	public void setModuleCode(String moduleCode) {
+		this.moduleCode = moduleCode;
+	}
 }
