95b74bf6e9d35da250d85559a01828be81baa793
Alex Sidorov <lex.rodis@gmail.com>
2023-11-30 14:05:59 +0000

    -

--- AcademicSystem.java
+++ AcademicSystem.java
@@ -6,30 +6,29 @@ public class AcademicSystem {
     private Map<Integer, Transcript> studentTranscripts;
     private List<Department> departments;
 
-    public AcademicSystem(List<Department> departments) {
+    public AcademicSystem() {
         this.studentTranscripts = new HashMap<>();
-        this.departments = departments;
     }
 
-    public void generateTranscript(Student student) {
+    public void addDepartment(Department department) {
+        departments.add(department);
+    }
+
+    public void addStudent(Student student) {
         Transcript transcript = new Transcript(student);
-        transcript.generateTranscript();
         studentTranscripts.put(student.getStudentId(), transcript);
     }
 
-    public Transcript getStudentTranscript(Student student) {
-        return studentTranscripts.get(student.getStudentId());
+    public double calculateOverallAverageQCA(Student student) {
+        Transcript transcript = studentTranscripts.get(student.getStudentId());
+        return transcript.calculateQCA();
     }
 
-    public void submitResults(Faculty faculty, List<ModuleGrade> moduleGrades) {
-        faculty.submitResultsForModules(moduleGrades);
+    public boolean checkStudentProgression(Student student) {
+        return false;
     }
 
-    public void holdExamBoard(Department department, ExamBoard examBoard) {
-        department.holdExamBoard(examBoard);
-    }
-
-    public Transcript getFacultyTranscript(Faculty faculty) {
-        return null;
+    public boolean checkForRepeatModules(Student student) {
+        return false;
     }
 }
--- TestAcademicSystem.java
+++ TestAcademicSystem.java
@@ -1,32 +1,35 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import java.util.ArrayList;
-import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import java.util.List;
 
 public class TestAcademicSystem {
 
+    private AcademicSystem academicSystem;
+    private Department engineeringDept;
+    private Program program1;
+    private Program program2;
     private Module module1;
     private Module module2;
     private Module module3;
     private Module module4;
     private Module module5;
-    private Program program1;
-    private Program program2;
-    private Department engineeringDept;
     private Student student1;
     private Student student2;
+    private Student student3;
 
     @BeforeEach
-    void setUp() {
-        module1 = new Module("M101", "Mathematics", 5, 0, null);
-        module2 = new Module("P201", "Physics", 4, 0, null);
-        module3 = new Module("C907", "English", 4, 0, null);
-        module4 = new Module("C123", "Irish", 4, 0, null);
-        module5 = new Module("C145", "Biology", 4, 0, null);
+    public void setUp() {
+        // Create Modules
+        module1 = new Module("M101", "Mathematics", 5);
+        module2 = new Module("P201", "Physics", 4);
+        module3 = new Module("C907", "English", 4);
+        module4 = new Module("C123", "Irish", 4);
+        module5 = new Module("C145", "Biology", 4);
 
+        // Create Programs
         program1 = new Program("LM121", "Bachelor of Science in Computer Science", "Engineering Dept", "Undergraduate", 4);
         program1.addModule(module1);
         program1.addModule(module2);
@@ -41,10 +44,12 @@ public class TestAcademicSystem {
         program2.addModule(module4);
         program2.addModule(module5);
 
+        // Create Department
         engineeringDept = new Department("Engineering Department");
         engineeringDept.addProgram(program1);
         engineeringDept.addProgram(program2);
 
+        // Create Students
         student1 = new Student(1001, "Billy Leenane", "123@gmail.com", "123 Town street", program1);
         student1.addModuleGrade(module1, "A1");
         student1.addModuleGrade(module2, "B2");
@@ -54,44 +59,48 @@ public class TestAcademicSystem {
 
         student2 = new Student(1002, "Ainsley Alabi", "Ainsley@gmail.com", "123 City St", program2);
         student2.addModuleGrade(module1, "A2");
-        student2.addModuleGrade(module2, "B3");
+        student2.addModuleGrade(module2, "B2");
         student2.addModuleGrade(module3, "C1");
         student2.addModuleGrade(module4, "A1");
         student2.addModuleGrade(module5, "B1");
+
+        student3 = new Student(1003, "Eoghan O'Donovan", "Eoghan@gmail.com", "1223 United St", program1);
+        student3.addModuleGrade(module1, "A1");
+        student3.addModuleGrade(module2, "B2");
+        student3.addModuleGrade(module3, "C3");
+        student3.addModuleGrade(module4, "A1");
+        student3.addModuleGrade(module5, "B2");
+
+        // Create Academic System
+        academicSystem = new AcademicSystem();
+        academicSystem.addDepartment(engineeringDept);
+        academicSystem.addStudent(student1);
+        academicSystem.addStudent(student2);
+        academicSystem.addStudent(student3);
     }
 
     @Test
-    void testCalculateAverageQCAAndRepeatingModules() {
-        double averageQCAStudent1 = student1.calculateQCA();
+    public void testCalculateOverallAverageQCA() {
+        double averageQCAStudent1 = academicSystem.calculateOverallAverageQCA(student1);
+        double averageQCAStudent2 = academicSystem.calculateOverallAverageQCA(student2);
+        double averageQCAStudent3 = academicSystem.calculateOverallAverageQCA(student3);
+
         assertEquals(3.28, averageQCAStudent1, 0.01);
-
-        if (averageQCAStudent1 < 2.0) {
-            assertTrue(false, "Student 1 needs to repeat the year");
-        }
-
-        List<ModuleGrade> student1Grades = student1.getModules();
-        for (ModuleGrade grade : student1Grades) {
-            if (grade.getGrade().equals("D1") || grade.getGrade().equals("D2")) {
-                assertTrue(false, "Student 1 needs to repeat a module");
-                break;
-            }
-        }
-
-        double averageQCAStudent2 = student2.calculateQCA();
         assertEquals(3.24, averageQCAStudent2, 0.01);
-
-        if (averageQCAStudent2 < 2.0) {
-            assertTrue(false, "Student 2 needs to repeat the year");
-        }
-
-        List<ModuleGrade> student2Grades = student2.getModules();
-        for (ModuleGrade grade : student2Grades) {
-            if (grade.getGrade().equals("D1") || grade.getGrade().equals("D2")) {
-                assertTrue(false, "Student 2 needs to repeat a module");
-                break;
-            }
-        }
+        assertEquals(3.0, averageQCAStudent3, 0.01);
     }
 
+    @Test
+    public void testStudentProgression() {
+        assertTrue(academicSystem.checkStudentProgression(student1));
+        assertTrue(academicSystem.checkStudentProgression(student2));
+        assertFalse(academicSystem.checkStudentProgression(student3));
+    }
 
+    @Test
+    public void testRepeatModules() {
+        assertFalse(academicSystem.checkForRepeatModules(student1));
+        assertFalse(academicSystem.checkForRepeatModules(student2));
+        assertTrue(academicSystem.checkForRepeatModules(student3));
+    }
 }
