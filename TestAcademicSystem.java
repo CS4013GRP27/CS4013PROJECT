888b09dd445afe905d81daf793ac0167a22dbe78
Alex Sidorov <lex.rodis@gmail.com>
2023-11-30 18:12:54 +0000

    --

--- TestAcademicSystem.java
+++ TestAcademicSystem.java
@@ -22,14 +22,12 @@ public class TestAcademicSystem {
 
     @BeforeEach
     public void setUp() {
-        // Create Modules
         module1 = new Module("M101", "Mathematics", 5);
         module2 = new Module("P201", "Physics", 4);
         module3 = new Module("C907", "English", 4);
         module4 = new Module("C123", "Irish", 4);
         module5 = new Module("C145", "Biology", 4);
 
-        // Create Programs
         program1 = new Program("LM121", "Bachelor of Science in Computer Science", "Engineering Dept", "Undergraduate", 4);
         program1.addModule(module1);
         program1.addModule(module2);
@@ -49,7 +47,6 @@ public class TestAcademicSystem {
         engineeringDept.addProgram(program1);
         engineeringDept.addProgram(program2);
 
-        // Create Students
         student1 = new Student(1001, "Billy Leenane", "123@gmail.com", "123 Town street", program1);
         student1.addModuleGrade(module1, "A1");
         student1.addModuleGrade(module2, "B2");
@@ -57,21 +54,7 @@ public class TestAcademicSystem {
         student1.addModuleGrade(module4, "B3");
         student1.addModuleGrade(module5, "B2");
 
-        student2 = new Student(1002, "Ainsley Alabi", "Ainsley@gmail.com", "123 City St", program2);
-        student2.addModuleGrade(module1, "A2");
-        student2.addModuleGrade(module2, "B2");
-        student2.addModuleGrade(module3, "C1");
-        student2.addModuleGrade(module4, "A1");
-        student2.addModuleGrade(module5, "B1");
 
-        student3 = new Student(1003, "Eoghan O'Donovan", "Eoghan@gmail.com", "1223 United St", program1);
-        student3.addModuleGrade(module1, "A1");
-        student3.addModuleGrade(module2, "B2");
-        student3.addModuleGrade(module3, "C3");
-        student3.addModuleGrade(module4, "A1");
-        student3.addModuleGrade(module5, "B2");
-
-        // Create Academic System
         academicSystem = new AcademicSystem();
         academicSystem.addDepartment(engineeringDept);
         academicSystem.addStudent(student1);
@@ -103,4 +86,8 @@ public class TestAcademicSystem {
         assertFalse(academicSystem.checkForRepeatModules(student2));
         assertTrue(academicSystem.checkForRepeatModules(student3));
     }
-}
+
+   }
+
+
+
