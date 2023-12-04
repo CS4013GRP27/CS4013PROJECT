32b24ace7cb210a28b9a9d427ab68938e886b08d
DaraghRoche7 <150368425+DaraghRoche7@users.noreply.github.com>
2023-12-04 03:57:29 +0000

    Update LoginDetails.java

--- LoginDetails.java
+++ LoginDetails.java
@@ -16,19 +16,21 @@ public class LoginDetails {
         createStudentAccounts();
     }
 
-    private void createStudentAccounts() {
+   private void createStudentAccounts() {
         // Create student accounts
-        Course course1 = new Course("LM121", "Bachelor of Science in Computer Science", "Engineering Dept", "Undergraduate", 4, CourseType.UNDERGRADUATE, "w");
+        Course course1 = new Course("LM121", "Bachelor of Science in Computer Science", "Engineering Dept", "Undergraduate", 4, CourseType.UNDERGRADUATE);
         Student student1 = new Student(1, "StudentName1", "student1@example.com", "Address1", course1);
         // Add module grades to the student
-        student1.addModuleGrade(new ModuleGrade("ModuleCode1", "A1", 4));
-        // Add more students and their module grades similarly
+        Module module1 = new Module("M101", "Mathematics", 5, 0, null);
+        student1.addModuleGrade(module1, "A");
+        
         students.put(1, student1); // Associate student ID with Student instance
 
         // Set default passwords (based on IDs for demonstration purposes)
         studentPasswords.put(1, "StudentName1"); // Example student password
         // Add more passwords similarly
     }
+
 
     public HashMap<Integer, String> getStudentPasswords() {
         return studentPasswords;
