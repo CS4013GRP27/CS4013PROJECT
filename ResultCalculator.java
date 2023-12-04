26f536a102d05fdfd6d53eeb059a9342d5fed153
DaraghRoche7 <150368425+DaraghRoche7@users.noreply.github.com>
2023-12-04 11:38:30 +0000

    Update ResultCalculator.java

--- ResultCalculator.java
+++ ResultCalculator.java
@@ -2,6 +2,7 @@ import java.util.List;
 
 public class ResultCalculator {
 
+
 	//Using Qca formula of Sum of(QPV multiplied by Module Credits) all over Total number of module credits for semester
     public double calculateQcaOneSemester(List<ModuleGrade> moduleGrades) {
         double totalGradePoints = 0.0;
@@ -91,37 +92,55 @@ public class ResultCalculator {
         double qca = calculateQcaOneSemester(moduleGrades);
         return qca > 2.0; 
     }
+    
+    public boolean isFailingSemester(List<ModuleGrade> moduleGrades) {
+        for (ModuleGrade moduleGrade : moduleGrades) {
+            if (isFailingGrade(moduleGrade.getGrade())) {
+                return true; 
+            }
+        }
+        return false;
+    }
+
+    private boolean isFailingGrade(String grade) {
+        return grade.equals("F") || grade.equals("N") || grade.equals("NG");
+    }
+    
+    public boolean isQcaBelowTwo(List<List<ModuleGrade>> allSemesterGrades) {
+        double overallQCA = calculateQcaAllSemesters(allSemesterGrades);
+        return overallQCA < 2.0;
+    }
 
     private double convertGradeToQPV(String grade) {
         switch (grade) {
-            case "A1": 
-                return 4.0;
-            case "A2": 
-                return 3.6;
-            case "B1": 
-                return 3.2;
-            case "B2": 
-                return 3.0;
-            case "B3": 
-                return 2.8;
+            case "A1":
+            	return 4.0;
+            case "A2":
+            	return 3.6;
+            case "B1":
+            	return 3.2;
+            case "B2":
+            	return 3.0;
+            case "B3":
+            	return 2.8;
             case "C1": 
-                return 2.6;
+            	return 2.6;
             case "C2": 
-                return 2.4;
+            	return 2.4;
             case "C3": 
-                return 2.0;
+            	return 2.0;
             case "D1": 
-                return 1.6;
-            case "D2": 
-                return 1.2;
+            	return 1.6;
+            case "D2":
+            	return 1.2;
             case "F": 
-                return 0;
-            case "NG": 
-                return 0;
+            	return 0;
+            case "NG":
+            	return 0;
             case "N": 
-                return 0;
+            	return 0;
             default: 
-                return 0;
+            	return 0;
         }
     }
 }
