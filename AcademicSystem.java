20e16bb1f2a72c59053398f2e45f1eb7146b265a
DaraghRoche7 <150368425+DaraghRoche7@users.noreply.github.com>
2023-12-04 03:52:44 +0000

    Update AcademicSystem.java

--- AcademicSystem.java
+++ AcademicSystem.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -8,6 +9,7 @@ public class AcademicSystem {
 
     public AcademicSystem() {
         this.studentTranscripts = new HashMap<>();
+        this.departments = new ArrayList<>();
     }
 
     public void addDepartment(Department department) {
@@ -25,18 +27,44 @@ public class AcademicSystem {
     }
 
     public boolean checkStudentProgression(Student student) {
-        return false;
+        Transcript transcript = studentTranscripts.get(student.getStudentId());
+        ResultCalculator resultCalculator = new ResultCalculator();
+
+        Map<Module, String> moduleGrades = transcript.getModuleGrades();
+        List<ModuleGrade> moduleGradesList = new ArrayList<>();
+        for (Map.Entry<Module, String> entry : moduleGrades.entrySet()) {
+            ModuleGrade moduleGrade = new ModuleGrade(entry.getKey(), entry.getValue());
+            moduleGradesList.add(moduleGrade);
+        }
+
+        List<List<ModuleGrade>> allSemesterGrades = new ArrayList<>();
+        allSemesterGrades.add(moduleGradesList);
+
+        double overallQCA = resultCalculator.calculateQcaAllSemesters(allSemesterGrades);
+        return overallQCA >= 2.0;
     }
 
     public boolean checkForRepeatModules(Student student) {
+        Transcript transcript = studentTranscripts.get(student.getStudentId());
+
+        Map<Module, String> moduleGrades = transcript.getModuleGrades();
+        for (String grade : moduleGrades.values()) {
+            if (grade.equals("F") || grade.equals("N") || grade.equals("NG") || grade.equals("I")) {
+                return true;
+            }
+        }
+
         return false;
     }
 
-    public List<Student> getStudentsByProgram(Course course1) {
-        return null;
+    public List<Student> getStudentsByCourse(Course course) {
+        List<Student> students = new ArrayList<>();
+        // Logic to retrieve students by course
+        return students;
     }
 
     public String calculateOverallAverageQCA() {
+        // Logic to calculate overall average QCA for all students
         return null;
     }
 }
