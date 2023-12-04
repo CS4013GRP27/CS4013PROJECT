867d20876ab0c923861d45d1d6e2aa152f5d5aa1
DaraghRoche7 <150368425+DaraghRoche7@users.noreply.github.com>
2023-12-04 11:31:21 +0000

    Update TestAcademicSystem.java

--- TestAcademicSystem.java
+++ TestAcademicSystem.java
@@ -1,5 +1,6 @@
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
@@ -9,7 +10,7 @@ public class TestAcademicSystem {
     // Test cases for Module class
     @Test
     public void testModuleConstructor() {
-        Module module = new Module("M101", "Mathematics", 5, 0, null);
+        Module module = new Module("M101", "Mathematics", 5, "Underg", null);
         assertEquals("M101", module.getModuleCode());
         assertEquals("Mathematics", module.getTitle());
         assertEquals(5, module.getCredits());
@@ -17,7 +18,7 @@ public class TestAcademicSystem {
 
     @Test
     public void testModuleGetterMethods() {
-        Module module = new Module("M101", "Mathematics", 5, 0, null);
+        Module module = new Module("M101", "Mathematics", 5, "Postgraduate", null);
         assertEquals("M101", module.getModuleCode());
         assertEquals("Mathematics", module.getTitle());
         assertEquals(5, module.getCredits());
@@ -40,7 +41,7 @@ public class TestAcademicSystem {
     @Test
     public void testAddModuleToCourse() {
         Course course = new Course("COMP1010", "Programming Concepts", "Computer Science", "Undergraduate", 1, null);
-        Module module = new Module("M101", "Mathematics", 5, 0, null);
+        Module module = new Module("M101", "Mathematics", 5, "Postgraduate", null);
         course.addModule(module);
         
         List<Module> modulesOffered = course.getModulesOffered(); // Assuming getModulesOffered() returns a List<Module>
@@ -53,7 +54,7 @@ public class TestAcademicSystem {
     @Test
     public void testRemoveModuleFromCourse() {
         Course course = new Course("COMP1010", "Programming Concepts", "Computer Science", "Undergraduate", 1, null);
-        Module module = new Module("M101", "Mathematics", 5, 0, null);
+        Module module = new Module("M101", "Mathematics", 5, "Postgraduate", null);
         course.addModule(module);
         course.removeModule(module);
         
@@ -63,6 +64,28 @@ public class TestAcademicSystem {
         assertFalse(modulesOffered.contains(module));
     }
     
-    private void assertFalse(boolean b) {
+    @Test
+    public void testCalculateQcaOneSemester() {
+        ResultCalculator calculator = new ResultCalculator();
+
+        // Create Module instances
+        Module module1 = new Module("M101", "Mathematics", 5, "Postgraduate", null);
+        Module module2 = new Module("P201", "Physics", 4, "Undergraduate", null);
+        Module module3 = new Module("C907", "English", 4, "Postgraduate", null);
+
+        ModuleGrade grade1 = new ModuleGrade(module1, "A1", 5);
+        ModuleGrade grade2 = new ModuleGrade(module2, "B2", 4);
+        ModuleGrade grade3 = new ModuleGrade(module3, "C3", 6);
+
+        List<ModuleGrade> moduleGrades = new ArrayList<>();
+        moduleGrades.add(grade1);
+        moduleGrades.add(grade2);
+        moduleGrades.add(grade3);
+
+        double expectedQCA = (4.0 * 5 + 3.0 * 4 + 2.0 * 6) / (5 + 4 + 6);
+
+        assertEquals(expectedQCA, calculator.calculateQcaOneSemester(moduleGrades));
+    }
+    void assertFalse(boolean b) {
     }
 }
