2659ec7beb7f8bcbf870fb5855aaa602405c74fd
DaraghRoche7 <150368425+DaraghRoche7@users.noreply.github.com>
2023-12-04 03:53:48 +0000

    Update Course.java

--- Course.java
+++ Course.java
@@ -7,28 +7,30 @@ public class Course {
     private String department;
     private String level;
     private int duration;
-    private List<ModuleGrade> modulesOffered;
-    private ProgramType courseType;
-    private String courseFormat;
+    private List<Module> modulesOffered;
+    private List<ModuleGrade>moduleGrades;
+    private CourseType courseType;
+    private List<Student> enrolledStudents;
 
-    public Course(String courseCode, String name, String department, String level, int duration,
-                   ProgramType programType, String programFormat) {
+    public Course(String courseCode, String name, String department, String level, int duration, CourseType courseType) {
         this.courseCode = courseCode;
         this.name = name;
         this.department = department;
         this.level = level;
         this.duration = duration;
-        this.courseType = programType;
-        this.courseFormat = programFormat;
+        this.courseType = courseType;
         this.modulesOffered = new ArrayList<>();
+        this.moduleGrades = new ArrayList<>();
+        this.enrolledStudents = new ArrayList<>();
     }
 
-    public void addModuleGrade(Module module, String grade) {
-        this.modulesOffered.add(new ModuleGrade(module, grade));
+    public void addModuleGrade(Module module, String grade, int creditValue) {
+        ModuleGrade moduleGrade = new ModuleGrade(module, grade, creditValue);
+        moduleGrades.add(moduleGrade);
     }
 
-    public double calculateProgramQCA(ResultCalculator calculator) {
-        return calculator.calculateQcaOneSemester(modulesOffered);
+    public double calculateSemesterQCA(ResultCalculator calculator) {
+        return calculator.calculateQcaOneSemester(moduleGrades);
     }
 
     public String getCourseCode() {
@@ -39,11 +41,11 @@ public class Course {
         this.courseCode = courseCode;
     }
 
-    public String getName() {
+    public String getCourseName() {
         return name;
     }
 
-    public void setName(String name) {
+    public void setCourseName(String name) {
         this.name = name;
     }
 
@@ -71,56 +73,79 @@ public class Course {
         this.duration = duration;
     }
 
-    public List<ModuleGrade> getModulesOffered() {
+    public List<Module> getModulesOffered() {
         return modulesOffered;
     }
 
-    public void setModulesOffered(List<ModuleGrade> modulesOffered) {
+    public void setModulesOffered(List<Module> modulesOffered) {
         this.modulesOffered = modulesOffered;
     }
 
-    public ProgramType getCourseType() {
+    public CourseType getCourseType() {
         return courseType;
     }
 
-    public void setCourseType(ProgramType courseType) {
+    public void setCourseType(CourseType courseType) {
         this.courseType = courseType;
     }
+    
+    public void addModule(Module module) {
+        modulesOffered.add(module);
+    }
+    
+    public void removeModule(Module module) {
+        modulesOffered.remove(module);
+    }
+    
 
-    public String getCourseFormat() {
-        return courseFormat;
+    public Module getModuleByCode(String moduleCode) {
+        for (Module module : modulesOffered) {
+            if (module.getModuleCode().equals(moduleCode)) {
+                return module;
+            }
+        }
+        return null; 
     }
 
-    public void setCourseFormat(String courseFormat) {
-        this.courseFormat = courseFormat;
+    public boolean removeModuleByCode(String moduleCode) {
+        Module moduleToRemove = null;
+        for (Module module : modulesOffered) {
+            if (module.getModuleCode().equals(moduleCode)) {
+                moduleToRemove = module;
+                break;
+            }
+        }
+        if (moduleToRemove != null) {
+            modulesOffered.remove(moduleToRemove);
+            return true; 
+        }
+        return false; 
+    }
+
+    public void enrollStudent(Student student) {
+        student.setCourse(this); 
+        enrolledStudents.add(student);
+    }
+   
+    public List<Student> getStudentsByCourse() {
+        return enrolledStudents;
     }
 
     @Override
     public String toString() {
-        return "Program{" +
+        return "Course{" +
                 "courseCode='" + courseCode + '\'' +
                 ", name='" + name + '\'' +
                 ", department='" + department + '\'' +
                 ", level='" + level + '\'' +
                 ", duration=" + duration +
                 ", courseType=" + courseType +
-                ", courseFormat='" + courseFormat + '\'' +
                 ", modulesOffered=" + modulesOffered +
                 '}';
     }
-
-    public void addModule(Module module1) {
-    }
-
-    public Object getModules() {
-        return null;
-    }
-
-    public void removeModule(Module module) {
-    }
 }
 
-enum ProgramType {
+enum CourseType {
     UNDERGRADUATE,
     POSTGRADUATE
 }
