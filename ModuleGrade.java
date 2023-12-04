27de69045f5d5d0d4a48f54d9a4db8b547a1cab1
DaraghRoche7 <150368425+DaraghRoche7@users.noreply.github.com>
2023-12-04 11:37:31 +0000

    Update ModuleGrade.java

--- ModuleGrade.java
+++ ModuleGrade.java
@@ -1,46 +1,108 @@
-public class ModuleGrade {
-    private String grade;
-    private int creditValue;
-    private Module module;
-    private String moduleCode;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-    public ModuleGrade(Module module, String grade, int creditValue) {
-        this.module = module;
-        this.grade = grade;
-        this.creditValue = creditValue;
+public class AcademicSystem {
+    private Map<Integer, Transcript> studentTranscripts;
+    private List<Department> departments;
+    private List<Student> students;
+
+    public AcademicSystem() {
+        this.studentTranscripts = new HashMap<>();
+        this.departments = new ArrayList<>();
+        this.departments = new ArrayList<>();
+        this.students = new ArrayList<>();
     }
 
-    public ModuleGrade(Module module2, String grade2) {
+    public void addDepartment(Department department) {
+        departments.add(department);
     }
 
-    public ModuleGrade(String moduleCode2, String grade2, int credits) {
+    public void addStudent(Student student) {
+        students.add(student);
     }
 
-    public String getGrade() {
-        return grade;
+    public List<Student> getStudents() {
+        return students;
+    }
+    
+    public double calculateOverallAverageQCA(List<Student> students) {
+        ResultCalculator resultCalculator = new ResultCalculator();
+        double totalQCA = 0.0;
+        int totalStudents = students.size();
+
+        for (Student student : students) {
+            double studentQCA = resultCalculator.calculateQcaOneSemester(student.getModules());
+            totalQCA += studentQCA;
+        }
+
+        if (totalStudents == 0) {
+            return 0.0;
+        }
+
+        return totalQCA / totalStudents;
     }
 
-    public void setGrade(String grade) {
-        this.grade = grade;
+    public boolean checkStudentProgression(Student student) {
+        Transcript transcript = studentTranscripts.get(student.getStudentId());
+        ResultCalculator resultCalculator = new ResultCalculator();
+
+        Map<Module, String> moduleGrades = transcript.getModuleGrades();
+        List<ModuleGrade> moduleGradesList = new ArrayList<>();
+        for (Map.Entry<Module, String> entry : moduleGrades.entrySet()) {
+            ModuleGrade moduleGrade = new ModuleGrade(entry.getKey(), entry.getValue());
+            moduleGradesList.add(moduleGrade);
+        }
+
+        List<List<ModuleGrade>> allSemesterGrades = new ArrayList<>();
+        allSemesterGrades.add(moduleGradesList);
+
+        double overallQCA = resultCalculator.calculateQcaAllSemesters(allSemesterGrades);
+        return overallQCA >= 2.0;
     }
 
-    public int getCreditValue() {
-        return creditValue;
+    public boolean checkForRepeatModules(Student student) {
+        Transcript transcript = studentTranscripts.get(student.getStudentId());
+
+        Map<Module, String> moduleGrades = transcript.getModuleGrades();
+        for (String grade : moduleGrades.values()) {
+            if (grade.equals("F") || grade.equals("N") || grade.equals("NG") || grade.equals("I")) {
+                return true;
+            }
+        }
+
+        return false;
     }
 
-    public void setCreditValue(int creditValue) {
-        this.creditValue = creditValue;
+    public List<Student> getStudentsByCourse(Course course) {
+        List<Student> studentsInCourse = new ArrayList<>();
+
+        for (Student student : students) {
+            if (student.getCourse().equals(course)) {
+                studentsInCourse.add(student);
+            }
+        }
+
+        return studentsInCourse;
     }
 
-    public String getModuleCode() {
-        return moduleCode;
+    public double calculateOverallAverageQCA(Map<Integer, Student> students) {
+    	ResultCalculator resultCalculator = new ResultCalculator();
+    	double totalQCA = 0.0;
+        int totalStudents = 0;
+
+        for (Student student : students.values()) {
+            double studentQCA =resultCalculator.calculateQcaOneSemester(student.getModules());
+            totalQCA += studentQCA;
+            totalStudents++;
+        }
+
+        if (totalStudents == 0) {
+            return 0.0; 
+        }
+
+        return totalQCA / totalStudents;
     }
 
-    public void setModule(String moduleCode) {
-        this.moduleCode = moduleCode;
-    }
-
-    public Module getModule() {
-        return null;
-    }
 }
